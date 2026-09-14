061b109e813f2c880a7fc34614581cdf81a0cd25
Arsenbotik228 <147414597+Arsenbotik228@users.noreply.github.com>
2026-09-14 16:28:19 +0600

    Initial clean release for Pralix (com.pralix.scale.calc)

---
+++ android/app/src/main/kotlin/com/pralix/scale/calc/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.pralix.scale.calc
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/app/brand.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+const String kAppTitle = 'Pralix';
+const String kProductTagline = 'Architectural drawing scale ratio converter and blueprint dimensioner.';
+const bool kIsLight = false;
+
+const Color cBg = Color(0xFF0A0F1D);
+const Color cSurface = Color(0xFF131C31);
+const Color cEdge = Color(0xFF1F2D4E);
+const Color cAccent = Color(0xFF0EA5E9);
+const Color cAccent2 = Color(0xFF7DD3FC);
+const Color cInk = Color(0xFFF0F9FF);
+
+const String kFont = 'AppFont';
